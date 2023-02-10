49ee84b9bfb597cbbae9459de3f684653edd7421
paszgyul <paszgyu@gmail.com>
2023-02-10 09:02:21 +0100

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,42 +3,46 @@ const fs = require('fs');
 const path = require("path");
 const api = express.Router()
 
-api.use('/public', express.static(`${__dirname}/../frontend/public`));
+	//api.use('/public', express.static(`${__dirname}/../frontend/public`));
 
 	api
 	.route('/pizza')
 	.get((req, res) => {
 		const pizzaArray = JSON.parse(fs.readFileSync("pizza.json", "utf8"));
 		res.json(pizzaArray.pizzas);
-  		//res.sendFile(path.join(`${__dirname}/../../frontend/index.html`));
-
-	})
+  	})
 
 	api
 	.route('/allergen')
 	.get((req, res) => {
   		const allergenArray = JSON.parse(fs.readFileSync("allergen.json", "utf8"));
 		res.json(allergenArray.allergens);
-
-  		//res.sendFile(path.join(`${__dirname}/../../frontend/index.html`));
 	})
 
 	api
 	.route('/order')
 	.get((req, res) => {
   		const orderArray = JSON.parse(fs.readFileSync("order.json", "utf8"));
-		res.json(orderArray.orders);
-
-  		//res.sendFile(path.join(`${__dirname}/../../frontend/index.html`));
+		res.json(orderArray);
 	})
 	.post((req, res) => {
+		console.log(req.body)
 		try {
 			fs.writeFileSync('order.json', JSON.stringify(req.body));
-			// file written successfully
-		  } catch (err) {
+			res.json({"Done" : true})
+			} catch (err) {
 			console.error(err);
-		  }
+			}
 	})
+
+	// .post((req, res) => {
+	// 	try {
+	// 		fs.writeFileSync('order.json', JSON.stringify(req.body));
+	// 		// file written successfully
+	// 	  } catch (err) {
+	// 		console.error(err);
+	// 	  }
+	// })
 
 
 	
